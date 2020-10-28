00d825a1ccc24ad472ddb4a80fcf641a24cc4e5e
Chad Specter <chad.specter@gmail.com>
2020-10-28 09:30:58 -0400

    Added media queries components

--- pages/create_post/index.tsx
+++ pages/create_post/index.tsx
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { Desktop, Tablet, Mobile, Default } from '../../components/MediaQueries'
+import { StyleSheet, Text, Dimensions } from 'react-native';
+import { Button, Container, Content, Header, Left, Icon, Body, Title, Right, Item, Input, H1 } from 'native-base';
 
 const CreatePost = (props) => {
-    
 
     return (
+        <Container>
+            <Header>
 
+            </Header>
+            <Content>
+                <Desktop>
+
+                </Desktop>
+                <Tablet>
+
+                </Tablet>
+                <Mobile>
+                    
+                </Mobile>
+            </Content>
+        </Container>
     )
 }
